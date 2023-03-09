43aa5a5a8a382fffc37dc1eeba188b9d68793323
william0405 <s980011william@gmail.com>
2023-03-09 16:44:59 +0800

    week04

---
+++ week04-3_rotate_translate/main.cpp
@@ -0,0 +1,33 @@
+#include <GL/glut.h>
+float angle=0;
+void display()
+{
+        glClear(GL_COLOR_BUFFER_BIT|GL_DEPTH_BUFFER_BIT);
+        glPushMatrix();
+            glTranslatef(0.5,0,0);
+            glRotatef(angle,0,1,0);///旋轉中的藍色茶壺(自轉)
+            glColor3f(0,0,1);
+            glutSolidTeapot(0.3);
+        glPopMatrix();
+
+        glPushMatrix();
+
+            glRotatef(angle,0,1,0);
+            glTranslatef(0,0.5,0);///旋轉中的黃色茶壺(公轉)
+            glColor3f(1,1,0);
+            glTranslatef(0.5,0,0);
+            glutSolidTeapot(0.3);
+        glPopMatrix();
+        glutSwapBuffers();
+        angle++;
+}
+int main(int argc,char* argv[])
+{
+    glutInit(&argc,argv);
+    glutInitDisplayMode(GLUT_RGB|GLUT_DOUBLE|GLUT_DEPTH);
+    glutCreateWindow("week04");
+
+    glutDisplayFunc(display);
+    glutIdleFunc(display);
+    glutMainLoop();
+}
