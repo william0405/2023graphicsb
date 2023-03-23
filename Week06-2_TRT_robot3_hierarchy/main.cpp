8aa0b287dc8ba96438348c51e968880b71912702
william0405 <s980011william@gmail.com>
2023-03-23 16:22:44 +0800

    week06

---
+++ Week06-2_TRT_robot3_hierarchy/main.cpp
@@ -0,0 +1,48 @@
+#include <GL/glut.h>
+float angle=0;
+void myCube()
+{
+    glPushMatrix();
+        glScalef(1,0.3,0.3);
+        glutSolidCube(0.5);
+    glPopMatrix();
+
+}
+void display()
+{
+        glColor3f(1,1,1);
+        glClear(GL_COLOR_BUFFER_BIT|GL_DEPTH_BUFFER_BIT);
+
+        glutSolidSphere(0.01,30,30);
+
+        glPushMatrix();  ///右邊的
+            glTranslatef(0.25,0,0);
+            glRotatef(angle,0,0,1);
+            glTranslatef(0.25,0,0);
+            myCube(); ///上手臂
+            glPushMatrix();
+                glTranslatef(0.25,0,0);
+                glRotatef(angle,0,0,1);
+                glTranslatef(0.25,0,0);
+                myCube();///下手肘
+            glPopMatrix();
+        glPopMatrix();
+
+
+
+        glutSwapBuffers();
+        angle++;
+}
+int main(int argc,char* argv[])
+{
+    glutInit(&argc,argv);
+    glutInitDisplayMode(GLUT_RGB|GLUT_DOUBLE|GLUT_DEPTH);
+    glutCreateWindow("week06");
+
+    glutDisplayFunc(display);
+    glutIdleFunc(display);
+    glutMainLoop();
+}
+
+
+
