e75e87613d1f75240d62252a4bdc92f94ecc723b
william0405 <s980011william@gmail.com>
2023-04-27 16:31:51 +0800

    week11

---
+++ week11-3_PlaySound.cpp
@@ -0,0 +1,13 @@
+#include <windows.h>
+#include <stdio.h>
+int main()
+{
+    ///PlaySound("C:\\Users\\Administrator\\Desktop\\do-re-mi\\do.wav",NULL,SND_SYNC);
+    ///PlaySound("C:\\Users\\Administrator\\Desktop\\do-re-mi\\re.wav",NULL,SND_SYNC);
+    ///PlaySound("C:\\Users\\Administrator\\Desktop\\do-re-mi\\mi.wav",NULL,SND_SYNC);
+
+    PlaySound("do-re-mi/do.wav",NULL,SND_SYNC);
+    ///¬Û¹ï¸ô®|
+
+    printf("Hello World\n");
+}
