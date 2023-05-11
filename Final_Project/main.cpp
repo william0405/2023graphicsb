eeaf1435ad8d2c2910cf5fbea44e6c1c4be8e3d3
william0405 <s980011william@gmail.com>
2023-05-11 16:31:22 +0800

    week13

--- Final_Project/main.cpp
+++ Final_Project/main.cpp
@@ -31,41 +31,40 @@ void display()
         }
         glPushMatrix();
             glScalef(0.3 , 0.3 , 0.3);
-            if(show[0]) glmDraw(head , GLM_MATERIAL);
-            if(show[1])glmDraw(body , GLM_MATERIAL);
-            if(show[2])glmDraw(left_u_arm , GLM_MATERIAL);
-            if(show[3])glmDraw(left_d_arm , GLM_MATERIAL);
+            glPushMatrix();
+                glTranslated(teapotX , teapotY , 0);
+                if(show[0]) glmDraw(head , GLM_MATERIAL);
+            glPopMatrix();
+                if(show[1])glmDraw(body , GLM_MATERIAL);
+                if(show[2])glmDraw(left_u_arm , GLM_MATERIAL);
+                if(show[3])glmDraw(left_d_arm , GLM_MATERIAL);
         glPopMatrix();
         glutSwapBuffers();
 }
+int oldX=0 , oldY=0;
 void mouse(int button , int state , int x, int y)
 {
-    teapotX = (x-150)/150.0;        ///計算點擊位置的x
-    teapotY = (150-y)/150.0;        ///計算點擊位置的y
     if(state==GLUT_DOWN)
     {
-        if(fout==NULL) fout = fopen("file4.txt","w");
-
-        fprintf(fout, "%f %f\n",teapotX , teapotY);
+        oldX = x;
+        oldY = y;
     }
-    display();
 }
-///void keyboard(unsigned char key , int x ,int y)
-///{
-   /// if(fin==NULL)
-   /// {
-  ///      fclose(fout);
-  ///      fin = fopen("file4.txt", "r");
-  ///  }
-  ///  fscanf(fin , "%f%f", &teapotX , &teapotY);
- ///   display();
-///}
+void motion(int x, int y)
+{
+    teapotX += (x - oldX)/150.0;
+    teapotY -= (y-oldY)/150.0;
+    oldX = x;
+    oldY = y;
+    glutPostRedisplay();
+}
 int main(int argc, char** argv)
 {
     glutInit(&argc,argv);
     glutInitDisplayMode(GLUT_DOUBLE | GLUT_DEPTH);
     glutCreateWindow("week13");
 
+    glutMotionFunc(motion);
     glutDisplayFunc(display);
     glutKeyboardFunc(keyboard);
     glutMouseFunc(mouse);
