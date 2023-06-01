b547017bba4c9199dede5746624c0d058ee396bb
william0405 <s980011william@gmail.com>
2023-06-01 15:29:34 +0800

    week16

---
+++ week16_all/main.cpp
@@ -0,0 +1,82 @@
+#include <opencv/highgui.h> ///使用 OpenCV 2.1 比較簡單, 只要用 High GUI 即可
+#include <opencv/cv.h>
+#include <stdio.h>
+#include <GL/glut.h>
+#include "glm.h"
+GLMmodel * head = NULL;
+GLMmodel * body = NULL; ///GLMmodel * gundam = NULL;
+
+float teapotX = 0, teapotY = 0, angle = 0, angle2 = 0, oldX = 0, oldY = 0;
+int myTexture(char * filename)
+{
+    IplImage * img = cvLoadImage(filename); ///OpenCV讀圖
+    cvCvtColor(img,img, CV_BGR2RGB); ///OpenCV轉色彩 (需要cv.h)
+    glEnable(GL_TEXTURE_2D); ///1. 開啟貼圖功能
+    GLuint id; ///準備一個 unsigned int 整數, 叫 貼圖ID
+    glGenTextures(1, &id); /// 產生Generate 貼圖ID
+    glBindTexture(GL_TEXTURE_2D, id); ///綁定bind 貼圖ID
+    glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_S, GL_REPEAT); /// 貼圖參數, 超過包裝的範圖T, 就重覆貼圖
+    glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_T, GL_REPEAT); /// 貼圖參數, 超過包裝的範圖S, 就重覆貼圖
+    glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MAG_FILTER, GL_NEAREST); /// 貼圖參數, 放大時的內插, 用最近點
+    glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MIN_FILTER, GL_NEAREST); /// 貼圖參數, 縮小時的內插, 用最近點
+    glTexImage2D(GL_TEXTURE_2D, 0, GL_RGB, img->width, img->height, 0, GL_RGB, GL_UNSIGNED_BYTE, img->imageData);
+    return id;
+}
+void mouse(int button, int state, int x, int y) {
+    oldX = x;
+    oldY = y;
+}
+void motion(int x, int y) {
+    teapotX += (x - oldX) / 150.0 * 10; ///teapotX = (x-150)/150.0;
+    teapotY += (oldY - y) / 150.0 * 10; ///teapotY = (150-y)/150.0;
+    angle += x - oldX;
+    angle2 += oldY - y;
+    oldX = x;
+    oldY = y;
+    glutPostRedisplay();
+    printf("  glTranslatef( %.2f, %.2f, 0 ); \n", teapotX, teapotY );
+}
+void display() {
+    glClear(GL_COLOR_BUFFER_BIT|GL_DEPTH_BUFFER_BIT);
+    glPushMatrix();
+        glScalef(1.6, 1.6, 1.6);
+        glTranslatef(0, -0.5, 0);///往下一半
+        glPushMatrix();
+            glColor3f(1,1,1);
+            glScalef(0.04, 0.04, 0.04);
+            ///glRotatef(angle, 0, 1, 0);
+            glmDraw(body, GLM_MATERIAL|GLM_TEXTURE);///glmDraw(gundam, GLM_MATERIAL|GLM_TEXTURE);
+
+            glPushMatrix();
+                glTranslatef(-0.00, +22.53, 0 );
+                glRotatef(angle, 0, 1, 0);
+                glRotatef(angle2, 1, 0, 0);
+                glTranslatef( 0.00, -22.53, 0 );///glTranslatef(teapotX, teapotY, 0);
+                glmDraw(head, GLM_MATERIAL|GLM_TEXTURE);
+            glPopMatrix();
+        glPopMatrix();
+
+        glColor3f(0,1,0);
+        glutSolidTeapot( 0.02 );
+
+    glPopMatrix();
+    glutSwapBuffers();
+}
+
+int main(int argc, char** argv)
+{
+    glutInit(&argc, argv);
+    glutInitDisplayMode(GLUT_DOUBLE|GLUT_DEPTH);
+    glutCreateWindow("week16");
+
+    glutDisplayFunc(display);
+    glutMotionFunc(motion);
+    glutMouseFunc(mouse);
+
+    head = glmReadOBJ("model/head.obj");
+    body = glmReadOBJ("model/body.obj"); ///gundam = glmReadOBJ("model/Gundam.obj");
+    myTexture("model/Diffuse.jpg");
+    glEnable(GL_DEPTH_TEST);
+
+    glutMainLoop();
+}
